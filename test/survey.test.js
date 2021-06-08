ca385e887774f83d78226487c10c3099b8d244ed
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-06-08 11:19:04 +0800

    add additional test case

--- test/survey.test.js
+++ test/survey.test.js
@@ -1,24 +1,30 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-const chai                  = require('chai');
-const expect                = chai.expect;
-const SurveyModel           = require('../models/survey.model');
+const chai = require("chai");
+const expect = chai.expect;
+const SurveyModel = require("../models/survey.model");
 
+describe("Survey Model", function () {
+	it("Should return success message when input captcha matches.", function () {
+		let surveyModel = new SurveyModel();
+		let captcha = surveyModel.captcha;
+		let result = surveyModel.verifyCaptchaInput(captcha);
 
-describe("Survey Model", function(){
+		expect(result).to.equal("Success! Captcha input matched.");
+	});
 
-    it('Should return success message when input captcha matches.', function(){
-        let surveyModel = new SurveyModel();
-        let captcha = surveyModel.captcha;
-        let result = surveyModel.verifyCaptchaInput(captcha);
+	it("Should return success message again", function () {
+		let surveyModel = new SurveyModel();
+		let captcha = surveyModel.captcha;
+		let result = surveyModel.verifyCaptchaInput(captcha);
 
-        expect(result).to.equal("Success! Captcha input matched.");
-    });
+		expect(result).to.equal("Success! Captcha input matched.");
+	});
 
-    it('Should return error message when input captcha does not matched.', function(){
-        let surveyModel = new SurveyModel();
-        let result = surveyModel.verifyCaptchaInput("random");
+	it("Should return error message when input captcha does not matched.", function () {
+		let surveyModel = new SurveyModel();
+		let result = surveyModel.verifyCaptchaInput("random");
 
-        expect(result).to.equal("Error! Captcha input doesn't matched.");
-    });
+		expect(result).to.equal("Error! Captcha input doesn't matched.");
+	});
 });
